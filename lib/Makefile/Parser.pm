9ad2bccba47c8537492c8d19c19c88f557b25131
Yichun Zhang (章亦春) <agentzh@gmail.com>
2011-08-17 14:19:57 +0000

    Import of AGENT/Makefile-Parser-0.213 from CPAN.
    gitpan-cpan-distribution: Makefile-Parser
    gitpan-cpan-version:      0.213
    gitpan-cpan-path:         AGENT/Makefile-Parser-0.213.tar.gz
    gitpan-cpan-author:       AGENT
    gitpan-cpan-maturity:     released

--- lib/Makefile/Parser.pm
+++ lib/Makefile/Parser.pm
@@ -11,7 +11,7 @@ use Text::Balanced qw( gen_extract_tagged );
 
 #our $Debug = 0;
 our $Strict = 0;
-our $VERSION = '0.212';
+our $VERSION = '0.213';
 our $Error;
 our $Runtime = undef;
 
@@ -69,7 +69,7 @@ sub parse {
     my ($var, $value, $tar_name, $tar, $colon_type, $depends, $cmd);
     my @cmds;
     my %tars;
-    %$rvars = ();
+    #%$rvars = ();
     my $first_tar = 1;
     while (<$in>) {
         next if /^\s*#/;
@@ -883,7 +883,7 @@ Makefile::Parser - A simple parser for Makefiles
 
 =head1 VERSION
 
-This document describes Makefile::Parser 0.212 released on 17 August 2011.
+This document describes Makefile::Parser 0.213 released on 17 August 2011.
 
 =head1 SYNOPSIS
 
